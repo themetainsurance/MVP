c38de7732691c94c8268a5bf95ca088357710671
themetainsurance <314195515+themetainsurance@users.noreply.github.com>
2026-08-27 14:00:48 +0200

    Serve brand logo directly

--- app/components/BrandLogo.tsx
+++ app/components/BrandLogo.tsx
@@ -15,6 +15,7 @@ export default function BrandLogo({ inverse = false }: BrandLogoProps) {
         height={724}
         sizes="(max-width: 600px) 155px, 188px"
         className={`${styles.image} ${inverse ? styles.inverse : ""}`}
+        unoptimized
         priority
       />
     </span>
